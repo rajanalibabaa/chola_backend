6a3e4efaaff2e931c4c09b42f82f82ca37b9ff5d
rajanR14042000 <rajanravi144@gmail.com>
2025-12-23 13:28:47 +0530

    main worked files

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/*  server was checking*/
+/*   final work server was checking*/
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
