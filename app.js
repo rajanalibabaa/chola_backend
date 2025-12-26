fbf813e4253cd5fe9ed37081a4e2cfb60669c720
Aravind170425 <aravindbabaateam@gmail.com>
2025-12-26 13:56:52 +0530

    fine

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ import express from "express";
 import cors from "cors";
 import apiRoutes from "./src/routers/index.js";
 import cookieParser from "cookie-parser";
-// import { sendEmail } from "./src/config/email.js";
-// import { otpEmailTemplate } from "./src/template/otpEmailTemplate.js";
+
 
 const app = express();
 
@@ -16,12 +15,6 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-
-  // await sendEmail({
-  //     to: "aravind@20052001",
-  //     subject: "OTP Verification – MrFranchise",
-  //     html: otpEmailTemplate({ name: "Aravind", otp: "123456" }),
-  //   });
 
 
 app.use("/api", apiRoutes);
