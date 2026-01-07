784aa761cb7c6f1a9ec2c0e71cc870b813a7000b
AyanMrFranchies <ayankalaibabaateam@gmail.com>
2026-01-07 17:55:21 +0530

    coner cases

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:5173"],
+  origin: ["http://localhost:3000", "http://localhost:5173","https://eagleceramic.com/"],
   credentials: true
 }))
 app.use(express.json());
