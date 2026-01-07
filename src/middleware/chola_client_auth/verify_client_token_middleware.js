9efaf48ff9989fb103adf4cc047b2f3fa3ac6c11
AyanMrFranchies <ayankalaibabaateam@gmail.com>
2026-01-07 11:12:33 +0530

    verify_client_token

--- src/middleware/chola_client_auth/verify_client_token_middleware.js
+++ src/middleware/chola_client_auth/verify_client_token_middleware.js
@@ -9,6 +9,8 @@ export const verify_client_token = async (req, res, next) => {
       req.query?.token ||
       req.header("Authorization")?.replace("Bearer ", "");
 
+      console.log(token)
+
     if (!token) {
       return res.status(401).json(
         new ApiResponse(401, null, "Token not found, Unauthorized request")
--- src/routers/eagleCeramicRouter/eagle_ceramic_catalog_routers.js
+++ src/routers/eagleCeramicRouter/eagle_ceramic_catalog_routers.js
@@ -1,20 +1,21 @@
 import express from "express";
 import { deleteEagleCeramicCatalogById, eagleCeramicCatalogCreate, getAllEagleCeramicCatalogbyProduct, updateEagleCeramicCatalog } from "../../controllers/eagleCeramicController/eagle_ceramic-catalog.js";
 import { upload } from "../../middleware/multer/multer.js";
+import { verify_client_token } from "../../middleware/chola_client_auth/verify_client_token_middleware.js";
 
 
 export const eagleCeramicCatalogRouter = express.Router();
 
 
-eagleCeramicCatalogRouter.post("/eagle-ceramic/catalog/create",upload.fields([
+eagleCeramicCatalogRouter.post("/eagle-ceramic/catalog/create",verify_client_token,upload.fields([
     { name: "image", maxCount: 10 },
     { name: "pdf", maxCount: 10 },
   ]),eagleCeramicCatalogCreate)
 
 eagleCeramicCatalogRouter.get("/eagle-ceramic/catalog/get-by-product",getAllEagleCeramicCatalogbyProduct)
-eagleCeramicCatalogRouter.patch("/eagle-ceramic/catalog/update/:id",upload.fields([
+eagleCeramicCatalogRouter.patch("/eagle-ceramic/catalog/update/:id",verify_client_token,upload.fields([
     { name: "image", maxCount: 10 },
     { name: "pdf", maxCount: 10 },
   ]),updateEagleCeramicCatalog)
 
-eagleCeramicCatalogRouter.delete("/eagle-ceramic/catalog/delete/:id", deleteEagleCeramicCatalogById)
+eagleCeramicCatalogRouter.delete("/eagle-ceramic/catalog/delete/:id", verify_client_token,deleteEagleCeramicCatalogById)
